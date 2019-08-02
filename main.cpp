0c1140d82619572b1ce52f6dcb685c7cea57d101
Pavel Poltaves <dzhura17@yandex.ru>
2019-08-02 20:28:07 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
 using namespace std;
 
-   int main()
-   {
+int main()
+{
     unsigned  int a ;
     cout<<"Vasha zarplata($): ";
     cin >>a;
-    while (1){
-    if (a>1000)
+    while (1)
     {
-        if (a>1000000){
-        cout <<"Ti millioner!";
-        break;
+        if (a>1000)
+        {
+            if (a>1000000)
+            {
+                cout <<"Ti millioner!";
+                break;
+            }
+            cout <<"Horosho";
+            break;
         }
-        cout <<"Horosho";
+        cout <<"Rabotai bolshe";
         break;
-    }
-    cout <<"Rabotai bolshe";
-    break;
     }
     cout <<"...no ti molodec";
     return 0;
-   }
+}
